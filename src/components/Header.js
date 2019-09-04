d2ce78b9117d9f06b97e498df8adfb9e0194d76f
bratyslav <beginner.altavista@gmail.com>
2019-09-04 18:34:08 +0300

    fixed header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,17 @@ import Search from './Search';
 const Header = () => {
   return (
     <header>
-      <a href="https://www.mcdonalds.ua" title="McDonalds">
+      <div className="header">
+        <Search />
+        <Link className="header__link-to-basket" to="/basket" />
+      </div>
+      <a href="https://www.mcdonalds.ua" title="www.mcdonalds.ua">
         <img
           className="header__logo"
           src="https://www.mcdonalds.ua/content/ua/_jcr_content/logo/logo.img.png/1478708656902.png"
           alt="Mc Donalds logo"
         />
       </a>
-      <Search />
-      <Link className="header__link-to-basket" to="/basket" />
     </header>
   );
 };
